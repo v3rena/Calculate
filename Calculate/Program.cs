ee336ce84155f8e206a1ba283359b08769eac4d2
v3rena <v.poetzl@gmx.at>
2019-05-29 18:22:28 +0200

    fix the typo close #18

--- Calculate/Program.cs
+++ Calculate/Program.cs
@@ -9,7 +9,7 @@ namespace Calculate
 			// Declare variables and then initialize to zero
 			int num1 = 0; int num2 = 0;
 
-			// Display title as the C# console calculator app
+			// Display title as the C# console calculator app!
 			Console.WriteLine("Console Calculator in C#\r");
 			Console.WriteLine("------------------------\n");
 
